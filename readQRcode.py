6708f4de6974182cb20349eda6b8b5b2a67f397d
Admirim Korici <admirim_korici.fshnstudent@unitir.edu.al>
2021-10-13 12:11:32 +0200

    Changes

--- readQRcode.py
+++ readQRcode.py
@@ -1,10 +1,5 @@
-# Importing the required modules
 import pyqrcode  
-# data to be inserted in the QR Code
-data = 'https://www.codeunderscored.com/'
-# creating the QR Code object with the data
+data = input('Vendosni linkun e QRcode qe doni te lexoni')
 qr = pyqrcode.QRCode(data)
-# using the terminal method to get the text for QR code
 text = qr.terminal()
-# displaying the QR Code
 print(text)
